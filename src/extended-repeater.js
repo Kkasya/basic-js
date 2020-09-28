ddcf1cd5aa40eda0b98c62021c754e3275c9b109
Kkasya <kasya--k@yandex.by>
2020-09-28 10:41:36 +0300

    feat: add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  //throw new CustomError('Not implemented');
+  let str1 = String(str);
+  let addition = '';
+  if (options.addition !== undefined) addition = String(options.addition);
+  let separator = '+';
+  if (options.separator) separator = String(options.separator);
+  let additionSeparator = '';
+  if (options.addition !== undefined) {
+    if (options.additionSeparator) {
+      additionSeparator = String(options.additionSeparator);
+    } else additionSeparator = '';
+  }
+  let repeatTimes = 1;
+  if (options.repeatTimes) repeatTimes = Number(options.repeatTimes);
+  let additionRepeatTimes = 1;
+  if (options.additionRepeatTimes) additionRepeatTimes = Number(options.additionRepeatTimes);
+  let result = '';
+
+  for (let i = 0; i < repeatTimes; i++) {
+    result += str1;
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      result += addition;
+      if (j != additionRepeatTimes - 1) result += additionSeparator;
+    }
+    if (i != repeatTimes - 1) result += separator;
+  }
+  return result;
 };
   
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,22 @@
 const CustomError = require("../extensions/custom-error");
-
+let res = 0;
+let max = 0;
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if (arr.length === 0) {
+      res += 1;
+      max = (res > max)?res:max;
+    } else {
+      res += 1;
+      arr.forEach(elem => {
+        if (Array.isArray(elem)) {
+          this.calculateDepth(elem);
+        } else {
+          max = (res > max)?res:max;
+        }
+      });
+      res = 0;
+    }
+    return max;
   }
 };
