7ce647ef8198a6a51aaabb9e46825325974c587b
Kkasya <kasya--k@yandex.by>
2020-09-29 14:11:23 +0300

    fix: 1 error in array

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,22 +1,25 @@
 const CustomError = require("../extensions/custom-error");
-let res = 0;
-let max = 0;
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-    if (arr.length === 0) {
-      res += 1;
-      max = (res > max)?res:max;
-    } else {
-      res += 1;
-      arr.forEach(elem => {
-        if (Array.isArray(elem)) {
-          this.calculateDepth(elem);
-        } else {
-          max = (res > max)?res:max;
-        }
-      });
-      res = 0;
+    let res = 0;
+    let max = 0;
+    function count(arr)  {
+      if (arr.length === 0) {
+        res += 1;
+        max = (res > max) ? res : max;
+      } else {
+        res += 1;
+        arr.forEach(elem => {
+          if (Array.isArray(elem)) {
+            count(elem);
+          } else {
+            max = (res > max) ? res : max;
+          }
+        });
+        res = 1;
+      }
     }
-    return max;
+    count(arr);
+    return (max === 0)?1:max;
   }
 };
